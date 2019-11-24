bdb72b63e96cc8b52264984bad80beee56ecc160
John Watson <johnwatson484@hotmail.com>
2019-11-24 19:46:22 +0000

    Defunct compose removed

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 docker-compose \
--f docker-compose.yaml \
 -f ../mine-support-web/docker-compose.yaml \
 -f ../mine-support-web/docker-compose.message.yaml \
 -f ../mine-support-claim-service/docker-compose.yaml \
